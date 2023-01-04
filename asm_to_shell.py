06dd6b48544ea6bde0429c84c02a8e7579103fd4
0x44F <89411010+0x44F@users.noreply.github.com>
2023-01-04 19:12:36 +1300

    Add arch detection and breakdown of asm

--- asm_to_shell.py
+++ asm_to_shell.py
@@ -38,8 +38,36 @@ class ShellcodeConverter:
         # write the lengthened shellcode to the output file
         with open(self.shellcode_filename, "wb") as f:
             f.write(shellcode)
+			
+	def detect(self, disassembler="ndisasm"):
+        # disassemble the shellcode and extract the first instruction
+        output = subprocess.run([disassembler, "-b", "64", "-e", "0x0", "-", self.shellcode_filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode()
+        lines = [line for line in output.split("\n") if line]
+        instruction = lines[0].split("\t")[1].strip()
+        
+        # check the first byte of the instruction to determine the architecture
+        if instruction.startswith("0f"):
+            # two-byte instruction, likely x86-64
+            return "x86-64"
+        elif instruction.startswith("b8"):
+            # mov instruction, likely x86-32
+            return "x86-32"
+        else:
+            # unknown instruction, unable to determine architecture
+            return "unknown"
+		
+	def explain(self, disassembler="ndisasm", platform="elf64"):
+        output = subprocess.run([disassembler, "-b", "64", "-e", "0x0", "-", self.asm_filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode()
+        
+        lines = [line for line in output.split("\n") if line]
+        
+        for line in lines:
+            address, instruction, explanation = line.split("\t")
+            print(f"0x{address}: {instruction.strip()}\n   {explanation.strip()}\n")
 
+
+		
 # example usage
 # converter = ShellcodeConverter("asm.asm", "shellcode.bin")
-# converter.lengthen_shellcode(0x90, 1000)  # lengthens the shellcode to 1000 bytes by repeating the NOP instruction (0x90)
+# converter.lengthen_shellcode(0x90, 1000) - lengthens the shellcode to 1000 bytes by repeating the NOP instruction (0x90)
 
